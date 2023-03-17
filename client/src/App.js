a59d9dee91c99a1a12fd7ea121544562febfe2f4
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-03-17 15:48:14 +0530

    Setup Router

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PageRender from "./PageRender";
+
+function App() {
+  return (
+    <Router>
+      <input type="checkbox" id="theme" />
+      <div className="App">
+        <div className="main">
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:id" component={PageRender} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
