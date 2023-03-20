01e96f917a637bdef8b53bf330e0ca53dfa46eb6
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-03-20 16:19:02 +0530

    Redux & Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PageRender from "./PageRender";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
+          <Route exact path="/" component={Login} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:id" component={PageRender} />
         </div>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { login } from "../Redux/actions/authAction";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
-  return <div>Login</div>;
+  const initialState = { email: "", password: "" };
+  const [userData, setUserData] = useState(initialState);
+
+  const { email, password } = userData;
+
+  const [typePass, setTypePass] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(userData)
+    dispatch(login(userData));
+  };
+
+  return (
+    <div className="auth_page">
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-uppercase text-center mb-4">Social Media App</h3>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            name="email"
+            aria-describedby="emailHelp"
+            onChange={handleChangeInput}
+            value={email}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <div className="pass">
+            <input
+              type={typePass ? "text" : "password"}
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              onChange={handleChangeInput}
+              value={password}
+            />
+            <small onClick={() => setTypePass(!typePass)}>
+              {typePass ? "Hide" : "Show"}
+            </small>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-dark w-100"
+          disabled={email && password ? false : true}
+        >
+          Login
+        </button>
+
+        <div className="my-2">
+          You don't have an account?{" "}
+          <Link to="/register" style={{ color: "crimson" }}>
+            Register Now
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
